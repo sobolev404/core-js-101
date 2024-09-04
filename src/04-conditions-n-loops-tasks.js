4adc926c2cff27a8f3714303d5c9887488183d1a
Georgiy Sobolev <goshasobolev2003@gmail.com>
2024-09-04 17:44:16 +0300

    fix

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -131,10 +131,10 @@ function isTriangle(a, b, c) {
  */
 function doRectanglesOverlap(rect1, rect2) {
   const noOverlap =
-    rect1.left + rect1.width <= rect2.left ||
-    rect2.left + rect2.width <= rect1.left ||
-    rect1.top + rect1.height <= rect2.top ||
-    rect2.top + rect2.height <= rect1.top;
+      rect1.left + rect1.width <= rect2.left ||  
+      rect2.left + rect2.width <= rect1.left ||  
+      rect1.top + rect1.height <= rect2.top ||   
+      rect2.top + rect2.height <= rect1.top;  
   return !noOverlap;
 }
 
@@ -166,7 +166,7 @@ function doRectanglesOverlap(rect1, rect2) {
  */
 function isInsideCircle(circle, point) {
   const distance = Math.sqrt(
-    Math.pow(point.x - circle.center.x, 2) +
+      Math.pow(point.x - circle.center.x, 2) +
       Math.pow(point.y - circle.center.y, 2)
   );
   return distance < circle.radius;
